9047d804e5633bf72d105a93209275b1443e858b
Keiichiro Ono <keiono@gmail.com>
2024-08-05 21:08:54 -0700

    Panels are now dynamically added.

--- hello-world/src/HelloApp.tsx
+++ hello-world/src/HelloApp.tsx
@@ -1,15 +1,17 @@
 import { ComponentType, CyApp } from '@cytoscape-web/types'
-import HelloPanel from './components/HelloPanel'
 
 export const HelloApp: CyApp = {
   id: 'hello',
   name: 'Hello Cy World App',
   url: '',
-  componentNames: ['HelloPanel', 'SubPanel'],
   components: [
     {
       id: 'MenuExample',
       type: ComponentType.Menu,
+    },
+    {
+      id: 'HelloPanel',
+      type: ComponentType.Panel,
     },
   ],
 }
