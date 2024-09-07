0ea39163dd6c6feee710cc42f8acb601e98ffa23
Keiichiro Ono <keiono@gmail.com>
2024-09-07 01:39:51 -0700

    Description added

--- simple-panel/src/SimplePanelApp.tsx
+++ simple-panel/src/SimplePanelApp.tsx
@@ -3,6 +3,7 @@ import { ComponentType, CyApp } from '@cytoscape-web/types'
 export const SimplePanelApp: CyApp = {
   id: 'simplePanel',
   name: 'Simple Panel App',
+  description: 'Sample app to add a panel to Cytoscape Web',
   components: [
     {
       id: 'SimplePanel',
